faaf5473b7e09e6304fd2a10cb0434c28d2423eb
Fabio Hiroki <fabiothiroki@gmail.com>
2016-09-10 15:04:20 -0300

    Added service class

--- app/src/main/java/ui/main/MainActivity.java
+++ app/src/main/java/ui/main/MainActivity.java
@@ -1,7 +1,9 @@
-package com.test.sisu;
+package com.test.sisu.ui.main;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.test.sisu.R;
 
 public class MainActivity extends AppCompatActivity {
 
